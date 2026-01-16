20e7033656b7236a887ed5ef62af8df56756c763
Shubham Kumar <shubhamkumar34586@gmail.com>
2026-01-16 09:15:17 +0530

    fix: add upload.js for vercel serverless

---
+++ server/utils/upload.js
@@ -0,0 +1,17 @@
+// server/utils/upload.js
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "/tmp"); // ✅ Vercel compatible
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    cb(null, `${Date.now()}${ext}`);
+  },
+});
+
+const upload = multer({ storage });
+
+export default upload;
