2c3fe4e80d4deff792543c76a03c3dc9d411178e
Shubham Kumar <shubhamkumar34586@gmail.com>
2026-01-16 09:17:33 +0530

    final fix

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import upload from "../utils/upload.js";
 import { signupUser, loginUser } from "../controller/user-controller.js";
 import { uploadImage, getImage } from "../controller/image-controller.js";
 import {
